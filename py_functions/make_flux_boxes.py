1b473ff364a794ed9c25b9fccdb2d910d7722f4c
Nari M <nari.v.miller@gmail.com>
2024-08-30 17:10:37 -0700

    add basic data references and flux box outline

--- py_functions/make_flux_boxes.py
+++ py_functions/make_flux_boxes.py
@@ -78,7 +78,8 @@ def wrap_flux_box_streamlit(dft, selval_dict):
     height = selval_dict['model_shape']
     flag_model = selval_dict['model_type']
     list_of_tuplelists,ft,fst, height , L, H, XY, fst = make_into_area_streamlit(dft, flag_model = flag_model, height = height)
-    maxynot, eqlocx = plot_patches(list_of_tuplelists, dft, ft, L, H, XY, fst, height = height, flag_model =flag_model, newfig = False,flag_annot = False)
+    maxynot, eqlocx = plot_patches(list_of_tuplelists, dft, ft, L, H, XY, fst, height = height,
+        flag_model =flag_model, newfig = False,flag_annot = False)
     fig.set_size_inches(selval_dict['figwidth'], selval_dict['figheight'])
     buf = BytesIO()
     fig.savefig(buf, format="png")
@@ -165,7 +166,8 @@ def make_into_area_streamlit(df, flag_model= 'simple', height = 'auto' ):
     return list_of_tuplelists, ft, fst, height, L, H, XY, fst
 
 
-def plot_patches(list_of_tuplelist, df, ft, L, H, XY, fst,add_conc = 'auto',  height = 'auto', flag_model = 'simple', newfig = True, flag_annot = True, set_maxy = None, xoffset = 0):
+def plot_patches(list_of_tuplelist, df, ft, L, H, XY, fst,add_conc = 'auto',  height = 'auto', flag_model = 'simple',
+    newfig = True, flag_annot = True, set_maxy = None, xoffset = 0):
     if newfig:
         fig, ax = plt.subplots()
     else:
@@ -225,7 +227,7 @@ def plot_patches(list_of_tuplelist, df, ft, L, H, XY, fst,add_conc = 'auto',  he
                 if i == 2:
                     equals_locx = npp[1][0]-spacex
         # st.write("xy: ", maxy, adjx)
-        maxx = maxx+adjx
+    maxx = maxx+adjx
         # maxy = np.max(xy)
 
     frame1 = plt.gca()
