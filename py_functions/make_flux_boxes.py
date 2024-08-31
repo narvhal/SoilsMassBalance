98adc2483901d304d9ed2b030460f4536e9aab0a
Nari M <nari.v.miller@gmail.com>
2024-08-30 17:20:00 -0700

    add basic data references and flux box outline

--- py_functions/make_flux_boxes.py
+++ py_functions/make_flux_boxes.py
@@ -238,7 +238,7 @@ def plot_patches(list_of_tuplelist, df, ft, L, H, XY, fst,add_conc = 'auto',  he
         frame1 = plt.gca()
     else:
         st.write("TWO max x",maxx)
-        plt.xlim(0, maxx2*2+0.3 )
+        plt.xlim(0, maxx2*1.1+0.3 )
         plt.ylim(0, maxy*2+0.1 )
     frame1.axes.get_xaxis().set_visible(False)
     frame1.axes.get_yaxis().set_visible(False)
