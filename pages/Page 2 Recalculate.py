c02a04f60df9e0ad2498dc6a7904d52f5db35c98
Nari M <nari.v.miller@gmail.com>
2024-09-02 15:25:47 -0700

    add page with code to recalc

--- pages/Page 2 Recalculate.py
+++ pages/Page 2 Recalculate.py
@@ -238,14 +238,15 @@ if st.checkbox("Continue?"):
 
                 # dff = write_defaults_to_df2(df)
                 # dft, SD, N = set_up_df_for_flux_results3(dft,dff)  # calc inventory (depends on z)
-                SD, coarse_mass = modify_start_subsoil_coarse_seds(dft, selval_dict['Coarse_seds_subsurface'])
+                flag_coarse_subsurface = selval_dict['Coarse_seds_subsurface']
+                SD, coarse_mass = modify_start_subsoil_coarse_seds(dft, flag_coarse_subsurface)
         #         print('get_new_df_results_w_unc:  coarse_mass: ', coarse_mass, '\nSD: ', SD)
                 zl = []
                 dft['z_old'] = dft['z'].copy()
                 dft['coarse_mass_old'] = dft['coarse_mass'].copy()
         #         print(dft['coarse_mass_old'])
                 for j, vz in enumerate(dft['z']):
-                    zl.append(redef_uf(vz - vz*selval_dict['Coarse_seds_subsurface'])/100))
+                    zl.append(redef_uf(vz - vz*flag_coarse_subsurface/100))
                 dft['z'] =zl
                 dft['coarse_mass'] = coarse_mass
 
