d26f1804a7345b72bd610559a3d029a5f85e1a5d
Nari M <nari.v.miller@gmail.com>
2024-09-02 16:28:42 -0700

    add page with code to recalc

--- pages/Page 2 Recalculate.py
+++ pages/Page 2 Recalculate.py
@@ -16,14 +16,14 @@ from io import BytesIO
 st.set_page_config(layout="wide" )
 
 
-fn = "https://github.com/narvhal/SoilsMassBalance/raw/main/data_sources/df_all_Tables_vars_baseline.xlsx"
-df = pd.read_excel(fn)
-df = df[df['select_col'] != "zcol"].copy()  # not useful
+# fn = "https://github.com/narvhal/SoilsMassBalance/raw/main/data_sources/df_all_Tables_vars_baseline.xlsx"
+# df = pd.read_excel(fn)
+# df = df[df['select_col'] != "zcol"].copy()  # not useful
 
 
 fn = "https://github.com/narvhal/SoilsMassBalance/raw/main/data_sources/defaults_Tables.xlsx"
 df_default =  pd.read_excel(fn)
-
+df = df_default.copy()
 
 # toc = stoc()
 
@@ -189,16 +189,16 @@ if st.checkbox("Continue?"):
         dft = df[df['sample_id']== samp].copy()
         # FIND DEFAULT ROW:
         # Get values from dft, overwrite later
-        dft = dft[dft['select_col'] == 'Coarse_seds_subsurface'].copy()
-        dft = dft[dft["select_col_val"]==0].copy()
+        # dft = dft[dft['select_col'] == 'Coarse_seds_subsurface'].copy()
+        # dft = dft[dft["select_col_val"]==0].copy()
         dft['Coarse_seds_subsurface'] = 0
-        dft['z'] = dft['z_val'].copy()
-        dft['p_re'] = dft['p_re_val'].copy()
-        dft['p_br'] = dft['p_br_val'].copy()
-        dft['N'] = dft['N_val'].copy()
-        dft['coarse_mass'] = dft['coarse_mass_val'].copy()
-        dft['coarse_area'] = dft['coarse_area_val'].copy()
-        dft['br_E_rate'] = dft['br_E_rate_val'].copy()
+        # dft['z'] = dft['z_val'].copy()
+        # dft['p_re'] = dft['p_re_val'].copy()
+        # dft['p_br'] = dft['p_br_val'].copy()
+        # dft['N'] = dft['N_val'].copy()
+        # dft['coarse_mass'] = dft['coarse_mass_val'].copy()
+        # dft['coarse_area'] = dft['coarse_area_val'].copy()
+        # dft['br_E_rate'] = dft['br_E_rate_val'].copy()
 
 
 
