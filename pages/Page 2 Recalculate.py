42da833ec533941c18e78928f930b591e3ff9b13
Nari M <nari.v.miller@gmail.com>
2024-09-02 16:10:24 -0700

    add page with code to recalc

--- pages/Page 2 Recalculate.py
+++ pages/Page 2 Recalculate.py
@@ -196,6 +196,7 @@ if st.checkbox("Continue?"):
         dft['p_re'] = dft['p_re_val'].copy()
         dft['N'] = dft['N_val'].copy()
         dft['coarse_mass'] = dft['coarse_mass_val'].copy()
+        dft['coarse_area'] = dft['coarse_area_val'].copy()
 
         AZ_D_graly = D_graly(400, 31.2)
         SP_D_graly = D_graly(510, 39.1)
