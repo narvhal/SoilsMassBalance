35e5bf7e014901df7e8d61b2b37495752321e873
Nari M <nari.v.miller@gmail.com>
2024-09-02 20:26:30 -0700

    add page with code to recalc

--- py_functions/make_flux_boxes.py
+++ py_functions/make_flux_boxes.py
@@ -1204,8 +1204,57 @@ def simple_recalc(dft, selcolkey, selval_dict, varvalues_dict, varnames_dict2, v
 
 
 def display_massbalance_equations():
-    tx = r''' DF = F_{dis} / F_{fb}
-                '''
+    tx = r'''\begin{equation}
+        DF = F_{dis} / F_{fb}
+            \label{eq:dissolutionfactorbalance}
+        \end{equation}
+        Substituting Equation \ref{eq:dissolutionfactorbalance} into Equation \ref{eq:fluxes_balance_nodust} yields:
+        \begin{equation} \label{eq:intermediate_derivation_mass_balance}
+            F_b = F_c + F_{fb}*(1+DF)
+        \end{equation}
+        Rearrange to isolate $F_{fb}$:
+        \begin{equation}
+           \label{eq:intermediate2_derivation_mass_balance}
+            -F_{fb} = \frac{F_c -F_b}{(1+DF)}
+        \end{equation}
+
+        Add unknown dust flux ($F_d$) to both sides, where $F_{d}$ is the mass flux of aeolian material, of any origin outside the hillslope and rearrange again:
+
+        \begin{equation} \label{eq:unknown_dust_both_sides}
+        F_d = F_d+ F_{fb} + \frac{F_c -F_b}{(1+DF)}
+        \end{equation}
+
+        Note that we do not include the dissolution of dust in this model.
+
+        $F_f$ represents the total flux of fine sediment (<2mm) out of the control volume, which is the sum of fine sediments produced by bedrock and those deposited by dust:
+
+        \begin{equation} \label{eq:define_Ff}
+            F_f =   F_{d} + F_{fb}
+        \end{equation}
+
+        Then, substituting Equation \ref{eq:define_Ff} into the unknown dust flux in Equation \ref{eq:unknown_dust_both_sides} :
+
+        \begin{equation} \label{eq:fluxes_dust_isolated}
+             F_{d}  = F_f + \frac{F_c  -F_b}{(1+DF)}
+        \end{equation}
+
+
+        In Equation \ref{eq:fluxes_dust_isolated}, the measurable quantities are isolated on the right side. Equations \ref{eq:fluxes_balance_nodust} and \ref{eq:fluxes_dust_isolated} are represented in Figure \ref{fig:fluxes_balanced_boxes}. The fluxes of fine sediment, coarse sediment, and bedrock can be evaluated through a combination of techniques, briefly described below and in more detail in Sections \ref{sec:Ffo}, \ref{sec:Fb}, and \ref{sec:Fc}.
+
+
+        \begin{figure}[htbp!]
+            \centering
+            \includegraphics[width=0.75\linewidth]{src/texts_6_12/June_12_Ch4/Figures/ConceptFigs/Mass_balance_visual_Equations.png}
+            \caption[Conceptual Visualization of Mass Balance of Regolith]{Top: A visual representation of the mass balance between the fluxes in and out of the regolith control volume, Equation \ref{eq:fluxes_balance_nodust}. Lower: The addition of dust means that the proportion of fine sediments derived from bedrock ($F_{bf}$) to dissolved material decreases ($F_{dis}$).}
+            \label{fig:fluxes_balanced_boxes}
+        \end{figure}
+
+        The mass flux from bedrock is estimated from the \cosmoCl{} in bedrock.  Mass flux (g/cm$^2$/yr) is related to lowering rate of bedrock ($E_b$, L/T, mm/kyr) by the density (g/cm$^3$), and so the flux of material released by the bedrock as it erodes can be calculated.
+
+        \begin{equation}
+            F_b = E_b*\rho_b
+            \label{eq:F_br}
+        \end{equation}'''
     st.latex(tx)
 
 
