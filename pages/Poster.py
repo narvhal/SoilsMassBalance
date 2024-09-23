524746aa74c5e21f60d9626f665da41b5d100ac7
Nari M <nari.v.miller@gmail.com>
2024-09-22 22:41:54 -0700

    re-copied raw paths

--- pages/Poster.py
+++ pages/Poster.py
@@ -7,21 +7,21 @@ import matplotlib.pyplot as plt
 from py_functions.make_flux_boxes import *
 from io import BytesIO
 
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+# def displayPDF(file):
+#     # Opening file from file path
+#     with open(file, "rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    # Embedding PDF in HTML
-    pdf_display =  f"""<embed
-    class="pdfobject"
-    type="application/pdf"
-    title="Embedded PDF"
-    src="data:application/pdf;base64,{base64_pdf}"
-    style="overflow: auto; width: 100%; height: 100%;">"""
+#     # Embedding PDF in HTML
+#     pdf_display =  f"""<embed
+#     class="pdfobject"
+#     type="application/pdf"
+#     title="Embedded PDF"
+#     src="data:application/pdf;base64,{base64_pdf}"
+#     style="overflow: auto; width: 100%; height: 100%;">"""
 
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
+#     # Displaying File
+#     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 flag_gh = True  #  st.checkbox("Flag_gh ? ", key = "flag_gh_key", value = True)
@@ -33,4 +33,5 @@ else:
     fn = r"C:\Users\nariv\OneDrive\JupyterN\streamlit_local\SoilsMassBalance\data_sources\GSA_2024_poster_NMiller3.pdf"
 
 
-displayPDF(fn)
+#displayPDF(fn)#
+st.image(fn)
