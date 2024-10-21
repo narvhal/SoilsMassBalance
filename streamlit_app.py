9db5966ad5052a1cacf72a0aa4a83a1238ead417
Nari <nari.v.miller@gmail.com>
2024-10-21 14:08:07 -0700

    update streamlit_app.py page paths

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@
 import streamlit as st
 pages = {
     " ": [
-        st.Page("pages/main_page.py", title="About Me")
+        st.Page("main_page.py", title="About Me")
     ],
     "Web Apps": [
-        st.Page("pages/page_app_w_recalc.py", title="Interactive Mass Balance Model"),
+        st.Page("page_app_w_recalc.py", title="Interactive Mass Balance Model"),
     ],
 }
 
