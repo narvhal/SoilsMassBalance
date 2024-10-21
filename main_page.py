43ffd3c916532065bb48b9e16251a902b50408c7
Nari <nari.v.miller@gmail.com>
2024-10-21 13:24:54 -0700

    fix pages path

--- main_page.py
+++ main_page.py
@@ -18,7 +18,7 @@ pages = {
         st.Page("main_page.py", title="About Me")
     ],
     "Web Apps": [
-        st.Page("page_app_w_recalc.py", title="Interactive Mass Balance Model"),
+        st.Page("pages/page_app_w_recalc.py", title="Interactive Mass Balance Model"),
     ],
 }
 
