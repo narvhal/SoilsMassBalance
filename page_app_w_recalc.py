edaccb8dcc4f94fee1cda1685140b57677be1ff1
Nari <nari.v.miller@gmail.com>
2024-10-22 10:03:24 -0700

    streamlit navigation bar doesn't seem to appear on web... fixing st latex issue first

--- page_app_w_recalc.py
+++ page_app_w_recalc.py
@@ -201,13 +201,13 @@ elif tabb == f"Mass Balance Equations":   #st.container():
     # # Add default values
     # with st.expander(f"Details of Model"):
     if model_type == "simple":
-        st.write("Model A: Simple Mass Balance Model")
+        st.write(f"Model A: Simple Mass Balance Model")
         st.latex(r"F_b = F_c + F_f + F_{dis}")
     elif model_type == "carbbalance":
-        st.write("Carbonate Mass Balance:")
+        st.write(f"Carbonate Mass Balance:")
         st.latex(r"F_b* \% CO_{3,b}^{-2} = F_c* \% CO_{3,c}^{-2}  + F_f* \% CO_{3,f}^{-2} + F_{dis}* \% CO_{3,dis}^{-2}")
     elif model_type == "wdust":
-        st.write("Mass Balance Including dust, constrained by insoluble material mass balance")
+        st.write(f"Mass Balance Including dust, constrained by insoluble material mass balance")
         st.latex(r"F_b + {\color{olive}F_d} = {\color{grey}F_c} + {\color{red}F_{f}} + {\color{teal}F_{dis}}")
         st.write(f"where **$F_b$** is bedrock mass flux, **$F_d$** is dust mass flux, **$F_c$** is coarse fraction of sediment mass flux, **$F_{{dis}}$** is dissolved material mass flux, and **$F_f$** is the entire fine fraction of sediment. ")
         st.write(f"Note that:  ")
@@ -217,8 +217,8 @@ elif tabb == f"Mass Balance Equations":   #st.container():
         st.latex(r"X_b F_b + {\color{olive}X_d F_d} = {\color{grey}X_c F_c} + {\color{red}X_f F_f} + {\color{teal}X_{dis} F_{dis}}")
         st.write(f" where X represents the fraction of mass flux that is insoluble. Note that **$X_{{dis}}$** is 0, by definition (all dissolved material is soluble), so that term goes to 0. For each other component, the fraction of insoluble material can be determined by bulk geochemistry. ")
         st.write(f"By solving both the mass balance and the insoluble fraction mass balance for $F_d$, and setting them equal to each other, we arrive at an expression for $F_{{dis}}$. Then, $F_d$ can be found using the mass balance equation.")
-        st.write("Dissolved flux: ")
-        st.latex("{\color{teal}F_{dis}} = ({\color{grey}X_{c} F_{c}} + {\color{red} X_{f} F_{f}} - X_{b} F_{b})/{\color{olive}X_{d}}  - {\color{red}F_{f}} - {\color{grey}F_{c}} + F_{b} ")
+        st.write(f"Dissolved flux: ")
+        st.latex(r"{\color{teal}F_{dis}} = ({\color{grey}X_{c} F_{c}} + {\color{red} X_{f} F_{f}} - X_{b} F_{b})/{\color{olive}X_{d}}  - {\color{red}F_{f}} - {\color{grey}F_{c}} + F_{b} ")
     
 
 elif tabb == f"Interactive App":
