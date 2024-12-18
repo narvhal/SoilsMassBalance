968aa9742de9d27c047614f2176c1fe79c2eff8c
Nari <nari.v.miller@gmail.com>
2024-12-18 14:05:27 -0800

    add general webpage text to overview

--- Overview.py
+++ Overview.py
@@ -11,8 +11,29 @@ from streamlit_navigation_bar import st_navbar
 
 st.set_page_config( layout="wide" )
 
+st.header("Nari Miller, Ph.D.")
+st.subheader("Geomorphologist")
 
-st.header("Interactive Carbonate Regolith Mass Balance Model")
+# st.header("Interactive Carbonate Regolith Mass Balance Model")
+lc, rc = st.columns([0.5, 0.5])
+with lc: 
+
+    st.write(f"I defended my dissertation in geomorphology in July 2024. I am looking for a job that makes use of the varied skills I've developed in graduate school. I enjoy the creative aspects of science: combining data in new ways, addressing questions using different data visualizations, and discussions with others. I'm motivated by work that involves a component of service, such as science education or hazards research. ")
+
+with rc:
+    st.image(r"/mount/src/narvhal/images/fulls/website_face.jpg")
+
+
+
+st.subheader(f"**Recent Work**")
+
+st.write(f"I studied arid and semi-arid landscapes composed of carbonate rocks, which erode mechanically and chemically via dissolution. Chemical erosion increases with precipitation, but the magnitude and controls on this process are still being worked out. There are clearly unique karst features in high-precipitation zones, but it is unclear whether dissolution continues to shape carbonate landscapes at lower ranges of precipitation. ")
+
+st.write(f"I test whether carbonate landscapes in arid and semi-arid regions are shaped by chemical erosion. A first-order effect of chemical erosion is the reduction of sediment produced on the landscape that requires transport by gravity-driven forces. I compared new and previously published erosion rates to topographic metrics to assess whether there was a signal of lower sediment flux in regions with higher precipitation. ")
+
+st.write(f"I also constructed a mass balance model which solves for dissolved material and dust influx, which I implemented at an arid and a semi-arid site. I made a web app that allows the user to change the inputs to this mass balance model -- take a look: https://carbonate-regolith.streamlit.app/")
+
+st.subheader(f"The rest of this website")
 
 st.write("This is an interactive supplement to my Geological Society of America poster. ")
 st.write("Explore the effects of input variables on the dust and dissolved material fluxes. ")
@@ -21,7 +42,7 @@ st.write("Questions/Comments? Get in touch! nari.v.miller@gmail.com")
 lc, rc = st.columns([0.5, 0.5])
 
 with lc:
-    st.subheader(f"**Summary of Poster**")
+    st.subheader(f"**Research Summary**")
 
     # with st.expander(f"**Introduction**"):
     st.write(f"**Introduction**")
