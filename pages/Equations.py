f8265123c8da1777dcf2e114a42ac1e868222acb
Nari <nari.v.miller@gmail.com>
2025-05-05 12:18:58 -0700

    adjust column alignments

--- pages/Equations.py
+++ pages/Equations.py
@@ -28,7 +28,8 @@ with cc:
     st.write(f"Mechanical weathering of bedrock produces coarse (>2 mm) lithic clasts.  Windblown dust, accumulating in the regolith, adds to the fine sediment fraction ($F_{{fines}}$ = $F_{{dust}}$ + $F_{{fines, bedrock}}$).")
 
     st.subheader(f"**Conservation of Mass Equations**")
-
+lc, rc = st.columns([0.5, 0.5])
+with lc:
     st.write(f"Mass Balance Including dust, constrained by insoluble material mass balance")
     st.latex(r"\LARGE F_b + \textcolor{#deb887}{F_d} = \textcolor{#bc8f8f}{F_c} + \textcolor{#cd5c5c}{F_f} + {\color{teal}F_{dis}}")
     st.write(f"where **$F_b$** is bedrock mass flux, **$F_d$** is dust mass flux, **$F_c$** is coarse fraction of sediment mass flux, **$F_{{dis}}$** is dissolved material mass flux, and **$F_f$** is the entire fine fraction of sediment. ")
@@ -51,9 +52,9 @@ with rc:
     diag_name = r"/mount/src/soilsmassbalance/data_sources/Figure_Fluxes_Concepts_from_INKSCAPE_GSAPoster2.svg"
     st.image(diag_name,width = 400, caption="Conceptual diagram")
 
-lc, rc = st.columns([0.5, 0.5])
+cc, rc = st.columns([0.61,0.39])
 
-with lc:
+with cc:
     st.subheader(f"**Sample Sites**")
     st.write(f"Two carbonate hillslopes were chosen: one in arid south-eastern Arizona and one in semi-arid zone south-eastern Spain.")
     # with st.expander(f"**Conclusions**"):
@@ -61,7 +62,6 @@ with lc:
     st.write(f"For both arid and semi-arid sites, the total mass flux is composed of more than 60% dissolved material. Even in arid and semi-arid regions, carbonate rocks may be subject to significant chemical erosion. Future work could identify whether additional chemical erosion occurs during clast transport, or if mineral precipitation inhibits transport out of the hillslope system (McFadden, 2013). ")
     st.write(f"The proportion of dissolved flux at the arid site is less than at the other site.  Supports a direct correlation between chemical erosion and climate. More work quantifying chemical erosion in carbonate soils across aridity gradients would help test this hypothesis. ")
     st.write(f"Dust flux was greater at the arid site. Besides climate, different local and regional dust sources, as well as variations in topography, might affect local dust accumulation (McClintock et al., 2015).  ")
-
 
 with rc:
     url = r"/mount/src/soilsmassbalance/data_sources/GSA_2024_poster_NMiller_fontsfixed.pdf"
