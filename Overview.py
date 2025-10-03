12ec26b0ac428558d7e71d7b199323328bf0ac55
Nari Miller <narvhal@users.noreply.github.com>
2025-10-03 12:23:33 -0700

    Update Overview.py
    modified text

--- Overview.py
+++ Overview.py
@@ -18,7 +18,7 @@ st.subheader("Geomorphologist")
 lc, rc = st.columns([0.8, 0.2])
 with lc: 
 
-    st.write(f"I defended my dissertation in geomorphology in July 2024. I am looking for a job that makes use of the varied skills I've developed in graduate school. I enjoy the creative aspects of science: combining data in new ways, addressing questions using different data visualizations, and discussions with others. I'm motivated by work that involves a component of service, such as science education or hazards research. ")
+    st.write(f"I defended my dissertation in geomorphology in July 2024. I enjoy the creative aspects of science: combining and visualizing data in new ways and discussions with others, especially in the field. I'm motivated by work that involves a component of service, such as science education or hazards research. ")
 
 
     url = r"/mount/src/soilsmassbalance/data_sources/N_Miller_CV_25_10.pdf"
@@ -28,16 +28,16 @@ with lc:
 
     st.download_button(label ="Download CV",
                         data=PDFbyte,
-                        file_name="Miller_N_CV_2024.pdf",
+                        file_name="N_Miller_CV_25_10.pdf",
                         mime='application/octet-stream')    
 
     st.subheader(f"**Recent Work**")
 
-    st.write(f"I studied arid and semi-arid landscapes composed of carbonate rocks, which erode mechanically and chemically via dissolution. Chemical erosion increases with precipitation, but the magnitude and controls on this process are still being worked out. There are dramatic and unique karst features in high-precipitation zones, but it is unclear whether dissolution continues to shape carbonate landscapes at lower ranges of precipitation. My current research stems from an interest in how the total mass of sediment from carbonate hillslopes has been reduced by chemical erosion and augmented by windblown dust. ")
+    st.write(f"I studied arid and semi-arid landscapes composed of carbonate rocks, which erode mechanically and chemically via dissolution. Chemical erosion increases with precipitation, but the magnitude and controls on this process are still being worked out. There are dramatic and unique karst features in high-precipitation zones, but it is unclear whether dissolution continues to shape carbonate landscapes at lower ranges of precipitation. My current research addresses how chemical erosion and aeolian dust modify the caliber and composition of sediment exported from carbonate hillslopes. ")
 
-    st.write(f"I test whether carbonate landscapes in arid and semi-arid regions are shaped by chemical erosion. A first-order effect of chemical erosion is the reduction of sediment produced on the landscape that requires transport by gravity-driven forces. I compared new and previously published erosion rates to topographic metrics to assess whether there was a signal of lower sediment flux in regions with higher precipitation. I planned and executed research to quantify a mass balance of hillslope sediments, which allowed me to independently quantify chemical erosion. ")
+    st.write(f"Even carbonate landscapes in arid and semi-arid regions are altered by chemical erosion. A first-order effect of chemical erosion is the reduction of sediment produced by eroding bedrock, and so slopes may adjust to transport less sediment. I compared new and previously published erosion rates to topographic metrics to assess whether there was a signal of lower sediment flux in regions with higher precipitation. I planned and executed research to quantify a mass balance of hillslope sediments, which allowed me to independently quantify chemical erosion. ")
 
-    st.write(f"Most models of sediment production and transport ignore chemical erosion and dust accumulation. However, I found the mass balances for both sites – semi-arid Spain and more arid Arizona – indicate 1) that more than half of the eroded material (>60% of total mass flux) may be dissolved, which would significantly change the character of sediment deposits in the catchment, and 2) dust accumulation (~10%) is necessary to balance the mass fluxes. Since graduating, I constructed an interactive webapp to show how these results change with different inputs (this site). I have presented this work at the Geological Society of America conference in Anaheim, and am preparing it for publication. ")
+    st.write(f"Because chemical erosion and dust accumulation may be spatially variable, many models of sediment production and transport ignore these processes. However, I found the mass balances for both sites – semi-arid Spain and more arid Arizona – indicate 1) that more than half of the eroded material (>60% of total mass flux) may be dissolved, which would significantly change the character of sediment deposits in the catchment, and 2) dust accumulation (~10%) is necessary to balance the mass fluxes. Since graduating, I constructed an interactive webapp to show how these results change with different inputs (this site). I have presented this work at the Geological Society of America conference in Anaheim, and am preparing it for publication. ")
 
     ## Tell me more ! expansion. 
     # st.write(f"Much of my graduate work was supported by a grant awarded to an interdisciplinary group of geomorphologists, archeologists, and fire scientists. I did fieldwork in southeast Spain alongside a group of archeologists from ASU and University of Valencia, who were primarily studying the charcoal and pollen in layered sedimentary deposits. Carbonate rocks like limestone can chemically erode (i.e. dissolve) when exposed to rainwater; caves and cenotes are examples of landforms shaped by dissolution. ")
